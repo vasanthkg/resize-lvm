2fc5259b496e3e766b732de9e6014a21ec2aa6f8
Vasanth KG <vasanth_kg@hotmail.com>
2021-09-28 23:13:40 +0530

    Update resize.sh

--- resize.sh
+++ resize.sh
@@ -6,7 +6,7 @@ then
   size=$(sudo pvs | tail -1 | awk {'print $NF'} | cut -d'.' -f1 | sed 's/<//')
   #if [ $size -ne 0 ];
   #then
-    sudo lvextend -L +${size} ${partition}G || error "lvextend $partition failed"
+    sudo lvextend -L +${size}G ${partition} || error "lvextend $partition failed"
     sudo resize2fs $partition || error "resize2fs $partition failed"
     echo "lvextend/resize2fs completed !"
     df -h $partition | tail -n 1
