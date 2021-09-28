b63e04463382d243cdfa150a76a4caa873c1acb9
Vasanth KG <vasanth_kg@hotmail.com>
2021-09-28 23:05:05 +0530

    Update resize.sh

--- resize.sh
+++ resize.sh
@@ -4,13 +4,13 @@ partition=$(df -h / | grep -i lv | tail -1 | cut -d' ' -f1)
 if [ ! -z "$partition" ];
 then
   size=$(sudo pvs | tail -1 | awk {'print $NF'} | tr '[:lower:]' '[:upper:]')
-  if [ $size -ne 0 ];
-  then
+  #if [ $size -ne 0 ];
+  #then
     sudo lvextend -L +$size $partition || error "lvextend $partition failed"
     sudo resize2fs $partition || error "resize2fs $partition failed"
     echo "lvextend/resize2fs completed !"
     df -h $partition | tail -n 1
-  else
-    echo "The filesystem is already extented to max available.  Nothing to do!"
-  fi
+  #else
+  #  echo "The filesystem is already extented to max available.  Nothing to do!"
+  #fi
 fi
